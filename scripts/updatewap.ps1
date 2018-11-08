a8c839009a2d832e7cd378df391e7797888f1096
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2018-11-08 15:29:06 -0500

    Add files via upload

---
+++ scripts/updatewap.ps1
@@ -0,0 +1,11 @@
+﻿#Obtain ADFS Private IP Address
+$ADFSIP = (Resolve-DnsName ADFS).IpAddress
+
+#Create a backup file
+New-Item -ItemType Directory -Force -Path C:\Temp
+Copy-Item -Path "C:\Windows\System32\drivers\etc\hosts" -Destination "C:\temp\hosts" -Force
+
+Set-HostEntry sts.swiftsolves.com $ADFSIP
+
+# Restart Service
+Restart-Service "appproxysvc"
